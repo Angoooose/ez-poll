a0cec54b02ed3ff7c419881ec6afb4218f81e5a5
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-03-11 13:50:31 -0600

    Renaming app

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Header() {
     return (
         <header className="bg-neutral-900 w-full mb-10 p-3 text-2xl font-bold shadow-md flex justify-between items-center">
-            todo
+            ez-poll
             <a className="h-8" href="https://github.com/Angoooose" target="_blank">
                 <Image src={github} width={32} height={32}/>
             </a>
