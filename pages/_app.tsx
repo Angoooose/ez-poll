8cd65ea4433d3e027f8dd82789ee7c0aeaad3ff5
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-03-13 16:41:39 -0500

    Installing and configuring react-hot-toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../index.css';
 import type { AppProps } from 'next/app';
 import Header from '../components/Header/Header';
+import { Toaster } from 'react-hot-toast';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div className="bg-neutral-800 text-white h-screen w-full">
       <Header isAuthed={pageProps.isAuthed}/>
       <Component {...pageProps} />
+      <Toaster/>
     </div>
   );
 }
