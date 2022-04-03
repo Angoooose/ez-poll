8d169e509a7eb596c9d766b5ed5bd998d495a321
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-04-02 19:02:08 -0500

    Update Card.tsx

--- components/common/Card.tsx
+++ components/common/Card.tsx
@@ -2,11 +2,12 @@ import { FunctionComponent } from 'react';
 
 interface CardProps {
     maxWidth?: 'sm'|'md'|'lg',
+    className?: string,
 }
 
-const Card: FunctionComponent<CardProps> = ({ children, maxWidth }) => {
+const Card: FunctionComponent<CardProps> = ({ children, maxWidth, className }) => {
     return (
-        <div className={`m-4 bg-neutral-700 py-4 px-10 rounded-md shadow-md transition-all ${maxWidth ? `max-w-${maxWidth}` : 'w-fit'}`}>
+        <div className={`m-4 bg-neutral-700 py-4 px-10 rounded-md shadow-md transition-all ${maxWidth ? `max-w-${maxWidth}` : 'w-fit'} ${className}`}>
             {children}
         </div>
     );
