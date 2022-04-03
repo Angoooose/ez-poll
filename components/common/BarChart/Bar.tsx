125cf12302060baa1af4a08e77367f0ed36ec1af
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-04-02 19:02:34 -0500

    Fixing BarCart winning bar

--- components/common/BarChart/Bar.tsx
+++ components/common/BarChart/Bar.tsx
@@ -29,9 +29,9 @@ export default function Bar({ values, index }: BarProps) {
         <div className="h-full text-center mx-2 flex flex-col items-center justify-end" data-tip={values[index].name}>
             <div className={`bg-gradient-to-t shadow-md rounded-md w-8 transition-all flex flex-col items-center ${colors[index]}`} style={{ height: `calc(${height}% - 25px)` }}>
                 <div className="-mt-6 text-gray-400" >{values[index].votes}</div>
-                {values[index].votes === sortedValues[0].votes && values.every(v => v.votes !== 0) && <StarIcon className="w-5 mt-2 text-yellow-300"/>}
+                {values[index].votes === sortedValues[0].votes && !values.every(v => v.votes === values[index].votes) && <StarIcon className="w-5 mt-2 text-yellow-300"/>}
             </div>
-            <div className={`mt-2 w-16 whitespace-nowrap overflow-y-clip overflow-x-hidden text-ellipsis i ${values[index].votes === sortedValues[0].votes && values.every(v => v.votes !== 0) ? 'text-yellow-200 font-medium' : ''}`}>{values[index].name}</div>
+            <div className={`mt-2 w-16 whitespace-nowrap overflow-y-clip overflow-x-hidden text-ellipsis i ${values[index].votes === sortedValues[0].votes && !values.every(v => v.votes === values[index].votes) ? 'text-yellow-200 font-medium' : ''}`}>{values[index].name}</div>
             <ReactTooltip place="top" type="dark" effect="solid"/>        
         </div>
     );
