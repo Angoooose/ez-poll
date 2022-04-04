5a03e6c2131142b909effdc1aca4078f93446fc0
Angoose <68403129+Angoooose@users.noreply.github.com>
2022-04-04 17:28:48 -0500

    Update Voted.tsx

--- components/Poll/Voted.tsx
+++ components/Poll/Voted.tsx
@@ -13,7 +13,7 @@ export default function Voted({ choices, userChoice }: VotedProps ) {
         <div className="bg-neutral-700 shadow-md rounded-md px-6 py-4 my-4 flex justify-between">
             <div className="flex flex-col justify-center items-center w-1/3">
                 <h1 className="text-lg font-medium mb-2">Your vote:</h1>
-                <div className={`w-full flex flex-col justify-center items-center py-5 rounded-md shadow-sm bg-gradient-to-r ${optionColors[userChoice]}`}>{choices[userChoice].name}</div>
+                <div className={`w-full flex flex-col justify-center items-center text-center py-5 px-4 rounded-md shadow-sm bg-gradient-to-r ${optionColors[userChoice]}`}>{choices[userChoice].name}</div>
             </div>
             <BarChart values={choices}/>
         </div>
